cf195570377aa2061f6a4e1777c584b00b069cfa
Алексей Мильто <miltoalvit@gmail.com>
2020-11-20 19:05:51 +0300

    Add Norway flag

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace ConsoleApp3
             //PadikiFancy(4, 15, 10);
             //GermanFlag(4, 15, 10);
             //SwitzerlandFlag(2, 10, 21);
+            NorwayFlag(6, 9, 32);
             Console.ForegroundColor = ConsoleColor.White;
         }
 
@@ -123,121 +124,6 @@ namespace ConsoleApp3
         }
 
         static void SwitzerlandFlag(int flatPerFloor, int padikiCount, int floorCount)
-        {
-            Console.WriteLine($"We have a {floorCount}-storey house with {padikiCount} padiks and {flatPerFloor} flats per floor");
-            Console.WriteLine($"flats In Houses:{flatPerFloor * floorCount * padikiCount}");
-            //Нада ебануть проверку на минимальный размер флага
-            if (floorCount < 5 ||  padikiCount * flatPerFloor < 5)
-            {
-                Console.WriteLine("Minimum flag size is 5*5");
-            }
-            //флаг Швейцарии квадратный, значит нужно сделать проверку на квадратность (ну тут конечно норкоманея но работает)
-            int noLitColomn =0;
-            int noLitFloor =0;
-
-            if (floorCount == padikiCount * flatPerFloor && floorCount % 5 == 0 && padikiCount * flatPerFloor % 5 == 0)
-            {
-                Console.WriteLine("All right bitches. Light it all.");
-            }
-            if (floorCount % 5 != 0)
-            {
-                if (floorCount > padikiCount * flatPerFloor)
-                {
-                    noLitFloor = floorCount - ((padikiCount * flatPerFloor) / 5) * 5;
-                }
-                else
-                {
-                    noLitFloor = floorCount - ((floorCount) / 5) * 5;
-                }
-                Console.WriteLine($"Your first {noLitFloor} floor(s) is(are) not lit");
-            }
-            if (padikiCount * flatPerFloor % 5 != 0)
-            {
-                if (floorCount > padikiCount * flatPerFloor)
-                {
-                    noLitColomn = padikiCount * flatPerFloor - ((floorCount) / 5) * 5;
-                }
-                else
-                {
-                    noLitColomn = padikiCount * flatPerFloor - ((padikiCount * flatPerFloor) / 5) * 5;
-                }
-                Console.WriteLine($"Your first {noLitColomn} colomn(s) is(are) not lit");
-            }
-            if (floorCount % 5 == 0 && padikiCount * flatPerFloor % 5 == 0 && floorCount > padikiCount * flatPerFloor)
-            {
-                noLitFloor = floorCount - padikiCount * flatPerFloor;
-                
-                Console.WriteLine($"Your first {noLitFloor} floor(s) is(are) not lit");
-            }
-            if (floorCount % 5 == 0 && padikiCount * flatPerFloor % 5 == 0 && floorCount < padikiCount * flatPerFloor)
-            {
-                noLitColomn = padikiCount * flatPerFloor - floorCount;
-
-                Console.WriteLine($"Your first {noLitColomn} floor(s) is(are) not lit");
-            }
-            //Ща буит долго и немного унизительно
-            int indexX = 1;
-            int indexY = 1;
-            for (; indexY <= floorCount; indexY++)
-            {
-                for (; indexX <= padikiCount * flatPerFloor; indexX++)
-                {
-                    if (noLitFloor != 0 && indexY <= noLitFloor)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Gray;
-                        Console.Write("*");
-                        Console.Write(" ");
-                    }
-                    else
-                    {
-                        if (noLitColomn != 0 && indexX <= noLitColomn)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Gray;
-                            Console.Write("*");
-                        }
-                        else
-                            if ((indexY <= (floorCount - noLitFloor) / 5 + noLitFloor) || (indexY > (floorCount - noLitFloor) * 4 / 5 + noLitFloor))
-                                {
-                                    Console.ForegroundColor = ConsoleColor.Red;
-                                    Console.Write("*");
-                                 }
-                            else if ((indexY > (floorCount - noLitFloor) * 2/5 + noLitFloor) && (indexY <= (floorCount - noLitFloor) * 3 / 5 + noLitFloor))
-                                 {
-                                       if ((indexX > (padikiCount * flatPerFloor - noLitColomn) * 1 / 5 + noLitColomn) && (indexX <= (padikiCount * flatPerFloor - noLitColomn) * 4 / 5 + noLitColomn))
-                                          {
-                                               Console.ForegroundColor = ConsoleColor.White;
-                                               Console.Write("*");
-                                          }
-                                       else
-                                          {
-                                              Console.ForegroundColor = ConsoleColor.Red;
-                                              Console.Write("*");
-                                           }
-                            }
-                            else
-                                   {
-                                      if ((indexX > (padikiCount * flatPerFloor - noLitColomn) * 2 / 5 + noLitColomn) && (indexX <= (padikiCount * flatPerFloor - noLitColomn) * 3 / 5 + noLitColomn))
-                                         {
-                                         Console.ForegroundColor = ConsoleColor.White;
-                                         Console.Write("*");
-                                         }
-                                       else
-                                          {
-                                            Console.ForegroundColor = ConsoleColor.Red;
-                                            Console.Write("*");
-                                          }
-                                   }
-                             Console.Write(" ");
-                    }
-                        
-                }
-                indexX = 1;
-                Console.WriteLine();
-            }
-            //Кто касавчик? Я красавчик
-        }
-
-        static void NorwayFlag(int flatPerFloor, int padikiCount, int floorCount)
         {
             Console.WriteLine($"We have a {floorCount}-storey house with {padikiCount} padiks and {flatPerFloor} flats per floor");
             Console.WriteLine($"flats In Houses:{flatPerFloor * floorCount * padikiCount}");
@@ -349,7 +235,133 @@ namespace ConsoleApp3
                 indexX = 1;
                 Console.WriteLine();
             }
+            //Кто касавчик? Я красавчик
         }
 
+        static void NorwayFlag(int flatPerFloor, int padikiCount, int floorCount)
+        {
+            Console.WriteLine($"We have a {floorCount}-storey house with {padikiCount} padiks and {flatPerFloor} flats per floor");
+            Console.WriteLine($"flats In Houses:{flatPerFloor * floorCount * padikiCount}");
+            //Нада ебануть проверку на минимальный размер флага
+            if (floorCount < 16 || padikiCount * flatPerFloor < 27)
+            {
+                Console.WriteLine("Minimum flag size is 16*27");
+            }
+            //проверка на не горящие квартиры
+            int noLitColomn = 0;
+            int noLitFloor = 0;
+
+            if (floorCount / 16 == padikiCount * flatPerFloor / 27 && floorCount % 16 == 0 && padikiCount * flatPerFloor % 27 == 0)
+            {
+                Console.WriteLine("All right bitches. Light it all.");
+            }
+            if (floorCount % 16 != 0)
+            {
+                if (floorCount / 16 > padikiCount * flatPerFloor / 27)
+                {
+                    noLitFloor = floorCount - ((padikiCount * flatPerFloor) / 27) * 27;
+                }
+                else
+                {
+                    noLitFloor = floorCount - ((floorCount) / 16) * 16;
+                }
+                Console.WriteLine($"Your first {noLitFloor} floor(s) is(are) not lit");
+            }
+            if (padikiCount * flatPerFloor % 27 != 0)
+            {
+                if (floorCount > padikiCount * flatPerFloor)
+                {
+                    noLitColomn = padikiCount * flatPerFloor - ((floorCount) / 16) * 16;
+                }
+                else
+                {
+                    noLitColomn = padikiCount * flatPerFloor - ((padikiCount * flatPerFloor) / 27) * 27;
+                }
+                Console.WriteLine($"Your first {noLitColomn} colomn(s) is(are) not lit");
+            }
+            if (floorCount % 16 == 0 && padikiCount * flatPerFloor % 27 == 0 && floorCount > padikiCount * flatPerFloor)
+            {
+                noLitFloor = floorCount - padikiCount * flatPerFloor;
+
+                Console.WriteLine($"Your first {noLitFloor} floor(s) is(are) not lit");
+            }
+            if (floorCount % 16 == 0 && padikiCount * flatPerFloor % 27 == 0 && floorCount/16 < padikiCount * flatPerFloor/27)
+            {
+                noLitColomn = padikiCount * flatPerFloor - floorCount;
+
+                Console.WriteLine($"Your first {noLitColomn} floor(s) is(are) not lit");
+            }
+            //Ща буит долго и немного унизительно
+            int indexX = 1;
+            int indexY = 1;
+            for (; indexY <= floorCount; indexY++)
+            {
+                for (; indexX <= padikiCount * flatPerFloor; indexX++)
+                {
+                    if (noLitFloor != 0 && indexY <= noLitFloor)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write("*");
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        if (noLitColomn != 0 && indexX <= noLitColomn)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            Console.Write("*");
+                        }
+                        else
+                        {
+
+                            if ((indexY <= (floorCount - noLitFloor) * 6 / 16 + noLitFloor) || (indexY > (floorCount - noLitFloor) * 10 / 16 + noLitFloor))
+                            {
+                                if ((indexX <= (padikiCount * flatPerFloor - noLitColomn) * 7 / 27 + noLitColomn) || (indexX > (padikiCount * flatPerFloor - noLitColomn) * 11 / 27 + noLitColomn))
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.Write("*");
+                                }
+                                else if ((indexX > (padikiCount * flatPerFloor - noLitColomn) * 8 / 27 + noLitColomn) && (indexX <= (padikiCount * flatPerFloor - noLitColomn) * 10 / 27 + noLitColomn))
+                                {
+                                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                                    Console.Write("*");
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.Write("*");
+                                }
+                            }
+                            else if ((indexY > (floorCount - noLitFloor) * 7 / 16 + noLitFloor) && (indexY <= (floorCount - noLitFloor) * 9 / 16 + noLitFloor))
+                            {
+
+                                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                                Console.Write("*");
+                               
+                            }
+                            else
+                            {
+                                if ((indexX > (padikiCount * flatPerFloor - noLitColomn) * 8 / 27 + noLitColomn) && (indexX <= (padikiCount * flatPerFloor - noLitColomn) * 10 / 27 + noLitColomn))
+                                {
+                                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                                    Console.Write("*");
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.Write("*");
+                                }
+                            }
+                            
+                        }
+                        Console.Write(" ");
+                    }
+
+                }
+                indexX = 1;
+                Console.WriteLine();
+            }
+
+        }
     }
 }
